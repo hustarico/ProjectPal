86d6fc5b07e876484af65154116e439a2763207d
hustarico <hussamtarteer74@gmail.com>
2026-05-07 01:43:24 +0300

    auth ACTUALLY works T-T

--- src/main/java/com/_4/ProjectPal/config/SecurityConfig.java
+++ src/main/java/com/_4/ProjectPal/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com._4.ProjectPal.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
